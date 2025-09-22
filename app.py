f1199e6681ce2fa1df9c65f9268d1556d074df00
Devtel0912 <devtel0912@gmail.com>
2025-09-21 21:46:12 -0400

    new code

--- app.py
+++ app.py
@@ -2,34 +2,16 @@ import webview
 
 
 class DreamAPI:
-    def start(self):
-        print("Lets Begin Your Dream Quiz!!!")
-        
-        
-        Q1 = input("What do you want to be when you grow up?")
-        
-        Q2 = input("Which school do you want to go to for your dream?")
-        
-        Q3 = input("How much will you earn from your chosen carrer?")
-        
-        while True:
-            Q4 = input("Q4: Do you have a backup career? (yes/no) ")
-            if Q4.strip().lower() == "yes":
-                print("Perfect")
-                break
-            elif Q4.strip().lower() == "no":
-                print("That's OK")
-                break
-            else:
-                print("Please answer 'yes' or 'no'.")
-            
-        
-        Q5 = bool("Do You truly believe in your chose carrer?")
+    @staticmethod
+    def start(self,answers):
+        Q1, Q2, Q3, Q4, Q5 = answers
+        Q4_response = "Perfect" if Q4.strip().lower() == "yes" else "That's OK"
         
         prediction = (
         f"Your chosen career is to be {Q1}.\n"
         f"The best school to graduate from is {Q2}.\n"
         f"Your expected salary will be {Q3}.\n"
+        f"{Q4_response} regarding a backup career.\n"
         f"Because you have chosen {Q5}.\n"
         "You have perfectly chosen your career and are set for life!"
 )
